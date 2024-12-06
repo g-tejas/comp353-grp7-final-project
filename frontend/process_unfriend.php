83a09496ccd3f947e2d016475152733edf44be41
heyitsarthur <mosnier.arthur.lfgeb@gmail.com>
2024-12-05 22:41:29 -0500

    unfriend feature

--- frontend/process_unfriend.php
+++ frontend/process_unfriend.php
@@ -15,14 +15,15 @@ $friend_id = $_POST['friend_id'];
 $query = "DELETE FROM member_relationship WHERE (Member_1_ID = ? AND Member_2_ID = ?) OR (Member_1_ID = ? AND Member_2_ID = ?)";
 $stmt = $conn->prepare($query);
 $stmt->bind_param("iiii", $member_id, $friend_id, $friend_id, $member_id);
+$stmt->execute();
 
-if ($stmt->execute()) {
-    echo "Friend removed successfully.";
-    header("Location: friends.php");
-    exit();
-} else {
-    echo "Error removing friend: " . $stmt->error;
-}
+// Delete the friendship from the profile_accissibility table
+$query = "DELETE FROM profile_accessibility WHERE (Member_ID = ? AND Target_ID = ?) OR (Member_ID = ? AND Target_ID = ?)";
+$stmt = $conn->prepare($query);
+$stmt->bind_param("iiii", $member_id, $friend_id, $friend_id, $member_id);
+$stmt->execute();
+
+header("Location: friends.php");
 
 $stmt->close();
 $conn->close();
